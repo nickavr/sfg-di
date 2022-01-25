21d8771dfed74726ceab1b5dee493ca9f320ef59
ZZ02SR826 <nicuavram11@gmail.com>
2022-01-25 23:18:31 +0200

    @Profile with multiple values and default profile

--- src/main/java/guru/springframework/sfgdi/services/I18nEnglishServiceImpl.java
+++ src/main/java/guru/springframework/sfgdi/services/I18nEnglishServiceImpl.java
@@ -4,7 +4,8 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
 @Service("i18nService") //naming the bean as we want, same as spanish version
-@Profile("EN") //setting a profile for spring to use in different scenarios; this is set in application.properties
+@Profile({"EN", "default"}) //setting a profile for spring to use in different scenarios; this is set in application.properties;
+// here I specified the default profile which will be used if no profile is set in the properties file
 public class I18nEnglishServiceImpl implements GreetingService{
     @Override
     public String sayGreeting() {
