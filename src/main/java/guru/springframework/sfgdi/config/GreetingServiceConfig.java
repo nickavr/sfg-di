2503716d54cb2e6717e0e6f7c5d77c1d5b5f3858
ZZ02SR826 <nicuavram11@gmail.com>
2022-02-13 22:24:20 +0200

    added DI in java configuration style, bean factory and bean scope examples

--- src/main/java/guru/springframework/sfgdi/config/GreetingServiceConfig.java
+++ src/main/java/guru/springframework/sfgdi/config/GreetingServiceConfig.java
@@ -1,17 +1,38 @@
 package guru.springframework.sfgdi.config;
 
+import com.springframework.pets.CatPetService;
+import com.springframework.pets.PetService;
+import com.springframework.pets.PetServiceFactory;
+import guru.springframework.sfgdi.repositories.EnglishGreetingRepository;
+import guru.springframework.sfgdi.repositories.EnglishGreetingRepositoryImpl;
 import guru.springframework.sfgdi.services.*;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.Profile;
-import org.springframework.stereotype.Service;
 
 /**
  * Created by jt on 2/20/21.
  */
 @Configuration
 public class GreetingServiceConfig {
+
+    @Bean
+    PetServiceFactory petServiceFactory(){
+        return new PetServiceFactory();
+    }
+
+    @Profile({"dog", "default"})
+    @Bean
+    PetService dogPetService(PetServiceFactory petServiceFactory){
+        return petServiceFactory.getPetService("dog");
+    }
+
+    @Profile("cat")
+    @Bean
+    PetService catPetService(PetServiceFactory petServiceFactory){
+        return petServiceFactory.getPetService("cat");
+    }
 
     @Bean
     ConstructorGreetingService constructorGreetingService(){
@@ -28,6 +49,9 @@ public class GreetingServiceConfig {
         return new SetterInjectedGreetingService();
     }
 
+    @Bean
+    EnglishGreetingRepository englishGreetingRepository(){return new EnglishGreetingRepositoryImpl(); }
+
     @Primary
     @Bean
     PrimaryGreetingService primaryGreetingService(){
@@ -36,7 +60,7 @@ public class GreetingServiceConfig {
 
     @Bean
     @Profile("EN")
-    I18nEnglishGreetingService i18nService() { return new I18nEnglishGreetingService(); }
+    I18nEnglishGreetingService i18nService(EnglishGreetingRepository englishGreetingRepository) { return new I18nEnglishGreetingService(englishGreetingRepository); }
 
     @Profile({"ES", "default"})
     @Bean("i18nService")
